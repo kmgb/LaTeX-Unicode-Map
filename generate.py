ff8bca6b086adc45e23ddd13c8cdfcdd211630db
kmgb <kevinmgb@proton.me>
2023-04-12 00:50:10 -0400

    Implement font variant parsing

---
+++ generate.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+from read_unicodedata import read_datafile, FontVariantType
+
+
+def main():
+    script_path = Path(__file__).parent
+    output_path = script_path / "output"
+    output_path.mkdir(exist_ok=True)
+
+    data = read_datafile()
+    superscripts = data.superscript_mapping
+    subscripts = data.subscript_mapping
+
+    write_to_file(output_path / "superscripts.txt", superscripts)
+    write_to_file(output_path / "subscripts.txt", subscripts)
+
+    font_variants = data.font_variants
+
+    variants = {
+        "mathbb": FontVariantType.DOUBLE_STRUCK | FontVariantType.MATHEMATICAL,
+        "mathbf": FontVariantType.BOLD | FontVariantType.MATHEMATICAL,
+        "mathbfcal": FontVariantType.BOLD | FontVariantType.SCRIPT | FontVariantType.MATHEMATICAL,
+        "mathbfit": FontVariantType.BOLD | FontVariantType.ITALIC | FontVariantType.MATHEMATICAL,
+        "mathbffrak": FontVariantType.BOLD | FontVariantType.FRAKTUR | FontVariantType.MATHEMATICAL,
+        "mathbfsfit": FontVariantType.BOLD | FontVariantType.SANS_SERIF | FontVariantType.ITALIC | FontVariantType.MATHEMATICAL,
+        # The difference between calligraphic and script types is not emphasized in Unicode, one could argue that this
+        # should be named mathscr instead (especially because of the forms of the lowercase characters) but I will
+        # leave it as is.
+        "mathcal": FontVariantType.SCRIPT | FontVariantType.MATHEMATICAL,
+        "mathfrak": FontVariantType.FRAKTUR | FontVariantType.MATHEMATICAL,
+        "mathit": FontVariantType.ITALIC | FontVariantType.MATHEMATICAL,
+        "mathsf": FontVariantType.SANS_SERIF | FontVariantType.MATHEMATICAL,
+        "mathsfit": FontVariantType.SANS_SERIF | FontVariantType.ITALIC | FontVariantType.MATHEMATICAL,
+        "mathtt": FontVariantType.MONOSPACE | FontVariantType.MATHEMATICAL,
+    }
+
+    for name, matches in variants.items():
+        write_to_file(
+            Path(output_path / f"{name}.txt"),
+            {k: x.text for k, v in font_variants.items() for x in v if x.kind == matches}
+        )
+
+
+def write_to_file(path: Path, data: dict[str, str]):
+    with path.open("w", encoding="utf-8") as f:
+        for key, value in data.items():
+            f.write(f"{key}={value}\n")
+
+
+if __name__ == "__main__":
+    main()
---
+++ read_unicodedata.py
@@ -0,0 +1,105 @@
+from dataclasses import dataclass
+from enum import IntFlag, auto
+import typing
+
+
+class FontVariantType(IntFlag):
+    NONE = 0
+    BOLD = auto()
+    DOUBLE_STRUCK = auto()
+    FRAKTUR = auto()
+    ITALIC = auto()
+    MATHEMATICAL = auto()
+    MONOSPACE = auto()
+    SANS_SERIF = auto()
+    SCRIPT = auto()
+
+
+@dataclass
+class CharacterFontVariant:
+    text: str
+    kind: FontVariantType
+
+
+class ParsedUnicodeData:
+    def __init__(self):
+        self.subscript_mapping: dict[str, str] = {}
+        self.superscript_mapping: dict[str, str] = {}
+        self.font_variants: dict[str, list[CharacterFontVariant]] = {}
+
+
+def read_datafile() -> ParsedUnicodeData:
+    result = ParsedUnicodeData()
+
+    # TODO: Fix finding superscript alpha, iota, epsilon
+    # Their fallbacks are listed as the "Latin" variants, meaning they aren't found
+    # when looking for ^{\alpha} as it looks for the Greek variants
+    with open("./UnicodeData.txt", encoding="utf-8") as f:
+        for line in f:
+            fields = line.split(";")
+            assert len(fields) == 15
+
+            codepoint = fields[0]
+            name = fields[1]
+            decomposition = fields[5]
+
+            char = chr(int(codepoint, 16))
+
+            if decomposition:
+                # Help out mypy with redefinitions
+                map_type: typing.Any
+                basechars: typing.Any
+
+                # print(f"{name} has decomposition {decomposition}")
+                *map_type, basechars = decomposition.split(maxsplit=1)
+
+                # We aren't looking for 2 -> 1 mappings, skip any that decompose to
+                # multiple characters.
+                basechars = basechars.split()
+                if len(basechars) > 1:
+                    continue
+
+                basechar = chr(int(basechars[0], 16))
+
+                assert len(map_type) < 2
+                map_type = "".join(map_type)
+
+                if map_type == "<super>":
+                    # Intentionally overwrite if there's multiple
+                    # Later unicode values tend to look more consistent with one another
+                    result.superscript_mapping[basechar] = char
+
+                elif map_type == "<sub>":
+                    # Intentionally overwrite if there's multiple
+                    result.subscript_mapping[basechar] = char
+
+                elif map_type == "<font>":
+                    variant = CharacterFontVariant(
+                        char,
+                        FontVariantType.MATHEMATICAL * ("MATHEMATICAL" in name)
+                        | FontVariantType.BOLD * ("BOLD" in name)
+                        | FontVariantType.DOUBLE_STRUCK * ("DOUBLE-STRUCK" in name)
+                        | FontVariantType.FRAKTUR * (any(x in name for x in ["FRAKTUR", "BLACK-LETTER"]))
+                        | FontVariantType.ITALIC * ("ITALIC" in name)
+                        | FontVariantType.MONOSPACE * ("MONOSPACE" in name)
+                        | FontVariantType.SANS_SERIF * ("SANS-SERIF" in name)
+                        | FontVariantType.SCRIPT * ("SCRIPT" in name)
+                    )
+
+                    result.font_variants.setdefault(basechar, []).append(variant)
+
+    # HACK: Fix up some missing mappings
+    result.superscript_mapping["α"] = "ᵅ"
+    result.superscript_mapping["ϵ"] = "ᵋ"
+    result.superscript_mapping["ι"] = "ᶥ"
+    result.superscript_mapping["ϕ"] = "ᶲ"
+
+    # Planck's constant already fulfills this role, but isn't detected
+    result.font_variants["h"].append(
+        CharacterFontVariant(
+            text="\u210E",
+            kind=FontVariantType.ITALIC | FontVariantType.MATHEMATICAL
+        )
+    )
+
+    return result
